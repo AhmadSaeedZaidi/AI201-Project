e2af938cb5c15131ae4679ee0093c1c9eea58c06
Ahmad Saeed Zaidi <ahmadsaeedzaidi@gmail.com>
2024-12-05 19:09:34 +0500

    file containing new structure

---
+++ project.cs
@@ -0,0 +1,20 @@
+project/
+│
+├── main.py
+├── ui/
+│   └── layout.py
+├── preprocessing/
+│   └── validate.py
+├── 2d/
+│   ├── explicit.py
+│   ├── implicit.py
+│   ├── parametric.py
+├── 3d/
+│   ├── explicit/
+│   │   ├── planes.py
+│   │   ├── surfaces.py
+│   ├── implicit/
+│   │   └── implicit_plot.py
+│   ├── parametric/
+│       ├── curves.py
+│       └── surfaces.py
